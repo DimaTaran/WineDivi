eed02a431ea13d3f926a8c50f444ec049e52c46d
Dmitry Taran <taran_da@ukr.net>
2026-02-11 20:13:22 +0200

    update the product-attributes template

--- woocommerce/single-product/product-attributes.php
+++ woocommerce/single-product/product-attributes.php
@@ -12,9 +12,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.6.0
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 9.3.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -22,18 +22,21 @@ defined( 'ABSPATH' ) || exit;
 if ( ! $product_attributes ) {
 	return;
 }
+if ( ! $product ) {
+	return;
+}
 ?>
-<table class="woocommerce-product-attributes shop_attributes">
+<table class="woocommerce-product-attributes shop_attributes" aria-label="<?php esc_attr_e( 'Product Details', 'woocommerce' ); ?>">
 	<?php foreach ( $product_attributes as $product_attribute_key => $product_attribute ) : ?>
 		<tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr( $product_attribute_key ); ?>">
-			<th class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
+			<th class="woocommerce-product-attributes-item__label" scope="row"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
 			<td class="woocommerce-product-attributes-item__value"><?php echo wp_kses_post( $product_attribute['value'] ); ?></td>
 		</tr>
 	<?php endforeach; ?>
-    <?php if ($custom_date= get_post_meta( $product->get_id(), 'expiry_date', true )) {
-        echo "<tr class='woocommerce-product-attributes-item'>
-        <th class='woocommerce-product-attributes-item__label'>Конечная дата:</th>
-            
-        <td class='woocommerce-product-attributes-item__value'>". $custom_date . " г</td>
-    </tr>"; } ?>
+	<?php if ($custom_date = get_post_meta( $product->get_id(), 'expiry_date', true )) : ?>
+		<tr class='woocommerce-product-attributes-item'>
+			<th class='woocommerce-product-attributes-item__label'><?php _e( 'Кінцева дата', 'woocommerce' ); ?>:</th>
+			<td class='woocommerce-product-attributes-item__value'><?php echo $custom_date  ?> г</td>
+		</tr>
+	<?php endif; ?>
 </table>
